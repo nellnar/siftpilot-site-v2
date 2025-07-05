a49cde02ea55b31c8dc328ce13a66672b67b263f
nellnar <neil@tysonhburridge.co.uk>
2025-07-05 15:37:46 +0100

    Create Footer.js

---
+++ src/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer style={{ padding: "10px", background: "#eee", textAlign: "center" }}>
+      <p>&copy; 2025 SiftPilot. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
