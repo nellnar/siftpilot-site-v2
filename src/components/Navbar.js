3d88bb6739714fde52dbad07a7d36eb3b868c9c1
nellnar <neil@tysonhburridge.co.uk>
2025-07-05 15:36:50 +0100

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav style={{ padding: "10px", background: "#eee" }}>
+      <h3>SiftPilot</h3>
+    </nav>
+  );
+}
+
+export default Navbar;
